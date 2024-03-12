c1f4597f3c67408d299f37f8cb529421fcab20bd
mostafa <mostafameerzad@gmail.com>
2024-03-12 10:42:39 +0430

    Create a simple counter app for testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
+import { useState } from "react";
+import "./App.css";
+
 const App = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const changeSing = () => {
+    setCount(count * -1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <main>
+      <h1 className="title">Counter</h1>
+
+      <p className="count">{count}</p>
+
+      <div className="btn-container">
+        <button className="btn" onClick={increment}>
+          +
+        </button>
+        <button className="btn" onClick={decrement}>
+          -
+        </button>
+        <button className="btn" onClick={changeSing}>
+          +/-
+        </button>
+        <button className="btn" onClick={reset}>
+          reset
+        </button>
+      </div>
+    </main>
+  );
+};
+export default App;
